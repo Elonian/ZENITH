819bcd34b52f029395ae231c84ba489afcfe4fec
Elonian <varun.sm@outlook.com>
2025-06-03 01:04:46 -0700

    minor fixes

--- agent/nav_agent.py
+++ agent/nav_agent.py
@@ -3,9 +3,12 @@ import traceback
 import numpy as np
 from simworld.agent.base_agent import BaseAgent
 from simworld.utils.vector import Vector
+from utils.generate_depth_map import generate_depth_from_img
 from utils.prompt_utils import WAYPOINT_GENERATION_PROMPT, WAYPOINT_SYSTEM_PROMPT
 # from simworld.traffic.base.traffic_signal import TrafficSignalState
 # from agent.action_space import Action, ActionSpace
+from PIL import Image
+import io
 
 class NavAgent(BaseAgent):
     _id_counter = 0
@@ -28,7 +31,7 @@ class NavAgent(BaseAgent):
         print(f"Agent {self.id} is running")
         try:
             self.navigate(exit_event)
-        except Exception e:
+        except Exception as e :
             print(f"Error in agent {self.id}: {e}")
             print(traceback.format_exc())
     
@@ -37,13 +40,19 @@ class NavAgent(BaseAgent):
 
         while (exit_event is None or not exit_event.is_set()):
             self.history.append(self.position) ## Adding the agent history
+            print(self.history)
+            print(hasattr(self.communicator, "get_camera_observation"))
             rgb_image = self.communicator.get_camera_observation(self.camera_id, 'lit')
-            try:
-                depth_image = self.communicator.get_camera_observation(self.camera_id, 'depth')
-            except Exception e:
-                print(f"Error in getting depth map for agent {self.id} with camera {self.camera_id}: {e}")
-                depth_image = self.communicator.generate_depth_model(rgb_image)
-            
+
+            # Already an image as a NumPy array
+            img_np = np.ascontiguousarray(rgb_image)
+            img = Image.fromarray(img_np)
+            img.show()
+            print(type(img))
+            # depth_image = self.communicator.get_camera_observation(self.camera_id, 'depth')
+
+            # depth_image = self.communicator.generate_depth_model(img)
+            print("Rgb image and depth image has been loaded")
             cam_info = self.communicator.get_camera_information(self.camera_id, rgb_image)
 
             current_yaw_rad = math.radians(self.yaw)
@@ -51,7 +60,6 @@ class NavAgent(BaseAgent):
             # Genarting naviagtable waypoints
             response = self.nav_llm.generate_waypoints_openai(
                 image = rgb_image,
-                depth_map = depth_image,
                 system_prompt = WAYPOINT_SYSTEM_PROMPT,
                 waypoint_prompt = WAYPOINT_GENERATION_PROMPT)
 
